c6dcc24f1f5265c4b6aa115c33a263ac9ce163d8
diniguez <iniguezdavid@gmail.com>
2026-02-22 14:56:49 +0100

    [djitello] add frontflip and backflip scripts

---
+++ djitello/frontflip.py
@@ -0,0 +1,16 @@
+from djitellopy import Tello
+import time
+
+tello = Tello()
+tello.connect()
+
+bateria = tello.get_battery()
+print(f"Nivel de batería: {bateria}%")
+
+tello.takeoff()
+time.sleep(2)
+
+tello.flip_forward()
+time.sleep(2)
+
+tello.land()
