47ff474c7df8dd0934f003ce957b073e6cfd5b16
diniguez <iniguezdavid@gmail.com>
2026-04-27 19:24:34 +0200

    [djitello] configure for drones connected to Wifi AP and environement instructions for Linux and Windows

---
+++ djitello/conexión/dron_a_traves_de_wifi.py
@@ -0,0 +1,25 @@
+from djitellopy import Tello
+import time
+
+# Datos de tu red Wi-Fi
+SSID = 'MerIA_Drones'
+PASSWORD = 'campeonatoriberabots26*'
+
+# Crear instancia del dron
+tello = Tello()
+
+# Conectar al dron (asegúrate de estar conectado al red wifi propia del dron)
+tello.connect()
+
+# Activar SDK (esto lo hace .connect() automáticamente, pero por seguridad lo repetimos)
+tello.send_control_command("command")
+
+# Enviar comando para configurar modo Router
+cmd = f"ap {SSID} {PASSWORD}"
+print(f"Enviando: {cmd}")
+tello.send_control_command(cmd)
+
+# Cerrar conexión
+tello.end()
+
+print("\n✅ Listo. Cambia el interruptor del dron a Router Mode y reinícialo.")
